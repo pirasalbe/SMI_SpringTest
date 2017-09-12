970f3efd030b66bf3b9008667b2a7d61787d0916
pirasalbe <alberto98.piras@gmail.com>
2017-09-12 08:57:09 +0200

    Test WS and WP

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,20 @@
+
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+
+	@RequestMapping("/greet")
+	public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
